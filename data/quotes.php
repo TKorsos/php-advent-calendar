02e93bb7950a90876d04eacbf3f2abcae6c3e49a
Tibor Korsos <tiborkorsos@gmail.com>
2023-12-05 21:21:55 +0100

    js addeventlistener css mod php mod

---
+++ data/quotes.php
@@ -0,0 +1,35 @@
+<?php
+
+// https://www.citatum.hu/kategoria/Lelek/11
+$idezet = [
+    ["quote" => "A szem mindent megmutat, még a lélek legapróbb titkait is.", "author" => "Julia RedHood"],
+    ["quote" => "A lelki sebek belül hagynak nyomot, olyan tintával, amely kitörölhetetlen.", "author" => "Charles Martin"],
+    ["quote" => "Nem elég, ha az ember csak kenyérrel él, valaminek a lelkét is táplálnia kell.", "author" => "William Shatner"],
+    ["quote" => "A szív a lélek mutatója.", "author" => "Hioszi Tatiosz"],
+    // 4
+    ["quote" => "Az ember nem test, hanem lélek: a test csak ruha; ha elszakad, újat osztanak a világ-magazinból.", "author" => "Gárdonyi Géza"],
+    ["quote" => "A test a mozgásból profitál, az elme a mozdulatlanságból.", "author" => "Szakjong Mipham Rinpocse"],
+    ["quote" => "A harcban nincsenek győztesek, csak túlélők vannak. A benned dúló harcot ki fogja túlélni?", "author" => "Ara Rauch"],
+    ["quote" => "Gyötrődik a lélek, ha lazsál mellette a test.", "author" => "Michel de Montaigne"],
+    // 8
+    ["quote" => "A belső béke nem elérhetetlen kincs, nem is luxus, hanem létszükséglet.", "author" => "Lontai Léna"],
+    ["quote" => "A lélek nem ismer időt.", "author" => "Rachel Lippincott"],
+    ["quote" => "A tudat (...) társadalmi termék, és az is marad, ameddig emberek egyáltalában léteznek.", "author" => "Karl Marx"],
+    ["quote" => "Ha a lélek egyszer elszáll, nincs orvosság, ami visszahozná.", "author" => "Jü Hua"],
+    // 12
+    ["quote" => "A művészet a lélek gyümölcse, de a lélek több a művészetnél.", "author" => "George Calinescu"],
+    ["quote" => "A lélek fekete éjszakájában mindig hajnali három óra van.", "author" => "Francis Scott Fitzgerald"],
+    ["quote" => "Az emberi léleknek bűvös ereje van, szalmából is gyémántot varázsolhat.", "author" => "Honoré de Balzac"],
+    ["quote" => "A szépség számomra az, hogyha valakinek a lelke nagyon szép.", "author" => "Balázs Andrea"],
+    // 16
+    ["quote" => "Ha egy lélek nem vágyik az életre, szinte lehetetlen megmenteni.", "author" => "Mo Xiang Tong Xiu"],
+    ["quote" => "A lélek mélyén lakozik valami megfoghatatlan és sérthetetlen.", "author" => "Anna Seghers"],
+    ["quote" => "Lelked hegedű, s te játszol rajta, és az első hallgatód önmagad vagy.", "author" => "Müller Péter"],
+    ["quote" => "A valódi béke az, amikor az elménk napi 24 órán át mentes a félelemtől és a szorongástól.", "author" => "Tendzin Gjaco"],
+    // 20
+    ["quote" => "A gyerekeknek nincs vesztenivalójuk, talán csak a lelkük.", "author" => "Sigrídur Hagalín Björnsdóttir"],
+    ["quote" => "Egy új sérülésre rámehet az ember teste, de egy régi felzabálhatja a lelkét is.", "author" => "Pierce Brown"],
+    ["quote" => "A javakban való szegény­séget könnyű orvosolni: a lélek szegénysé­gét lehetetlen.", "author" => "Michel de Montaigne"],
+    ["quote" => "A csodák mindig karácsonykor történnek. Csak ki kell várni.", "author" => "Matt Haig"],
+    // 24
+];
